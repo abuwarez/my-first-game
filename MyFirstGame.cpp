f46887653841d3e08394bff1bb9b5c657862e5fb
Ciprian <abuwarez@gmail.com>
2019-05-07 18:34:53 +0300

    Adding support for animations

--- MyFirstGame.cpp
+++ MyFirstGame.cpp
@@ -5,6 +5,10 @@
 #include "Model/Stone.h"
 #include "Model/Mover.h"
 
+#include <unistd.h>
+#include <ostream>
+#include <iostream>
+
 namespace MyGame
 {
 	class Settings
@@ -16,6 +20,90 @@ namespace MyGame
 		char rightKey;
 		char layBombKey;
 	};
+
+
+	class Animation
+	{
+		public:
+			virtual void Update(int elapsedMillis) = 0;//elapsed (delta) millis since last update
+			virtual void Reset() = 0;
+			virtual bool IsFinished() const = 0;
+			virtual ~Animation() {}
+	};
+
+
+	class StoneMover : public Animation
+	{
+		int mVelocityX; // pixels (terminal chars) per second
+		int mMaxX;
+
+		Stone & mStone;
+		int mElapsedTime;
+		int mInitialX;
+
+		public:
+			StoneMover(int vX, int maxX, Stone & stone) : mVelocityX(vX), mStone(stone), mElapsedTime(0), mInitialX(mStone.GetX()), mMaxX(maxX) {}
+
+			virtual void Update(int elapsedMillis) 
+			{
+				mElapsedTime += elapsedMillis;
+
+				mStone.SetX(mInitialX + mVelocityX * mElapsedTime / 1000);
+
+			}
+
+			virtual void Reset()
+			{
+				mElapsedTime = 0;
+				mInitialX = mStone.GetX();
+			}
+
+			virtual bool IsFinished() const
+			{
+				return mStone.GetX() == mMaxX;
+			}
+	};
+
+	class AnimationChain : public Animation
+	{
+		std::vector<Animation*> mAnimations;
+		int mActiveAnimIndex;
+
+		public:
+			AnimationChain() : mActiveAnimIndex(0) {}
+			
+			void AddAnimation(Animation* a)
+			{
+				mAnimations.push_back(a);
+			}
+
+			virtual void Update(int elapsedMillis)
+			{
+				Animation * activeAnim = mAnimations[mActiveAnimIndex];
+				if (!activeAnim->IsFinished())
+					activeAnim->Update(elapsedMillis);
+				else
+				{
+					mActiveAnimIndex = (mActiveAnimIndex + 1) % mAnimations.size();
+					Animation * activeAnim = mAnimations[mActiveAnimIndex];
+					activeAnim->Reset();
+					activeAnim->Update(elapsedMillis);
+				}
+			}
+
+			virtual bool IsFinished() const
+			{
+				return false;
+			}
+
+			virtual void Reset()
+			{
+				mActiveAnimIndex = 0;
+				Animation * activeAnim = mAnimations[mActiveAnimIndex];
+				activeAnim->Reset();
+			}
+	};
+
 }
 
 int main()
@@ -28,7 +116,8 @@ int main()
 	MyGame::Personaj personaj;
 	MyGame::Rectangle arena(maxX, maxY);
 
-	std::vector<MyGame::Stone> mStones(5);
+	std::vector<MyGame::Stone> mStones;
+	mStones.reserve(5);
 
 	mStones.push_back(MyGame::Stone(36, 13, 8, 4));
 	mStones.push_back(MyGame::Stone(13, 4, 9, 5));
@@ -36,10 +125,22 @@ int main()
 	mStones.push_back(MyGame::Stone(9, 20, 12, 8));
 	mStones.push_back(MyGame::Stone(40, 19, 20, 2));
 
+	MyGame::StoneMover stoneMoverRight(7, 72, mStones.front());
+	MyGame::StoneMover stoneMoverLeft(-7, 0, mStones.front());
+
+	MyGame::AnimationChain stoneAnimChain;
+	stoneAnimChain.AddAnimation(&stoneMoverRight);
+	stoneAnimChain.AddAnimation(&stoneMoverLeft);
+
 	personaj.SetX(70);
-	personaj.SetY(25);
+	personaj.SetY(13);
 
 	MyGame::Mover mover(personaj, arena, mStones);
+
+	int frameDurationMillis = 50;
+
+	std::vector<MyGame::Animation*> animations;
+	animations.push_back(&stoneAnimChain);
 
 	while (1)
 	{
@@ -51,8 +152,15 @@ int main()
 
 		s.Draw(personaj.GetShape(), personaj.GetX(), personaj.GetY());
 
+		std::cout << std::flush;
+
+		for (MyGame::Animation * anim : animations)
+			anim->Update(frameDurationMillis);
+
 		//input handling
-		char input = _getch();
+		char input = 0;
+		
+		_getch(input);
 
 		switch (input)
 		{
@@ -74,6 +182,10 @@ int main()
 		default:
 			break;
 		}
+
+		usleep(frameDurationMillis * 1000);
+
+
 	}
 }
 
--- System/KbInput.h
+++ System/KbInput.h
@@ -10,7 +10,7 @@
 #include <unistd.h>
 #include <stdio.h>
 
-char _getch()
+bool _getch(char & out)
 {
 	char buf = 0;
 	struct termios old = { 0 };
@@ -19,18 +19,24 @@ char _getch()
 		perror("tcsetattr()");
 	old.c_lflag &= ~ICANON;
 	old.c_lflag &= ~ECHO;
-	old.c_cc[VMIN] = 1;
+	old.c_cc[VMIN] = 0;
 	old.c_cc[VTIME] = 0;
 	if (tcsetattr(0, TCSANOW, &old) < 0)
 		perror("tcsetattr ICANON");
-	if (read(0, &buf, 1) < 0)
+	int readVal = read(0, &buf, 1);
+	if (readVal < 0)
 		perror("read()");
+	else if (readVal > 0)
+	{
+		out = buf;
+	}
+
 	old.c_lflag |= ICANON;
 	old.c_lflag |= ECHO;
 	if (tcsetattr(0, TCSADRAIN, &old) < 0)
 		perror("tcsetattr ~ICANON");
 	//printf("%c\n",buf);
-	return buf;
+	return readVal > 0;
 }
 
 #endif
